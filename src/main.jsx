a27fde92fc0fb7ed2f7b911894d0c004f488bfc0
Shravani Chendwankar <shravanichendwankar@gmail.com>
2024-08-25 08:47:35 +0530

    yes

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import "./scrollbar.css";
-import { ClerkProvider } from '@clerk/clerk-react';
-const { VITE_CLERK_PUBLISHABLE_KEY } = import.meta.env;
-
-const publishableKey = VITE_CLERK_PUBLISHABLE_KEY;
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ClerkProvider publishableKey={publishableKey}>
+  <React.StrictMode> 
       <App />
-    </ClerkProvider>
   </React.StrictMode>
 );
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,10 @@
-import { UserButton } from "@clerk/clerk-react";
+
 
 
 const Dashboard = () => {
     return (
       <div className="flex flex-col items-center justify-center p-10">
-            <h1 className="text-3xl font-semibold">👋Welcome to your dashboard!</h1>
-            <UserButton />
+           
       </div>
     );
 }
