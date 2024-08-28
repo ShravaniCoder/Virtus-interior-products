8757c6b2a96654e74cd97c20b4c0174dd6c74717
Shravani Chendwankar <132210516+ShravaniCoder@users.noreply.github.com>
2024-08-29 01:08:53 +0530

    Update List.jsx

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -6,7 +6,7 @@ import { toast } from "react-toastify";
 
 const List = () => {
 
-    const url = "http://localhost:4000";
+    const url = "https://virtus-interior-products-backend.onrender.com/";
     const [list, setList] = useState([]);
     const fetchList = async () => {
         const response = await axios.get(`${url}/api/project/list`);
