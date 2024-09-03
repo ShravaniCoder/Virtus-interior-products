37523864a3ee586e8c3d5b0d3722ab73ee0ead73
Shravani Chendwankar <shravanichendwankar@gmail.com>
2024-09-03 14:46:14 +0530

    yes

--- backend/routes/projectRoute.js
+++ backend/routes/projectRoute.js
@@ -5,11 +5,11 @@ import {
   listProject,
   removeProject,
 } from "../controllers/projectController.js";
-import { bucket } from "../firebase/firebase.js"; 
+import { bucket } from "../firebase/firebase.js";
 
 const projectRouter = express.Router();
 
-const storage = multer.memoryStorage(); 
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 projectRouter.post("/add", upload.single("image"), async (req, res) => {
@@ -27,14 +27,19 @@ projectRouter.post("/add", upload.single("image"), async (req, res) => {
   });
 
   blobStream.on("finish", async () => {
-    // Make the file public
-    await blob.makePublic();
+    try {
+      // Make the file public
+      await blob.makePublic();
 
-    // The public URL of the image
-    const publicUrl = `gs://virtus-interior.appspot.com/${bucket.name}/${blob.name}`;
+      // The public URL of the image
+      const publicUrl = `gs://virtus-interior.appspot.com/${bucket.name}/${blob.name}`;
 
-    // Call your addProject function with the public URL
-    await addProject(req, res, publicUrl);
+      // Call your addProject function with the public URL
+      await addProject(req, res, publicUrl);
+    } catch (error) {
+      console.error("Error making file public:", error);
+      res.status(500).send("Error making file public");
+    }
   });
 
   blobStream.end(file.buffer);
